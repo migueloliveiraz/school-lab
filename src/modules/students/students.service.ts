9943645b8a140ae156088fbbaa04db1e1b1ce767
migueloliveiraz <migueloliveiradev@gmail.com>
2022-07-09 15:33:40 -0300

    feat: add the students service

--- src/modules/students/students.service.ts
+++ src/modules/students/students.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { StudentsRepository } from './repositories/students.repository';
+
+import { StudentsRepositoryContract } from './contracts/students-repository.contract';
+import { CreateStudentDto } from './dtos/create-student.dto';
+import { UpdateStudentDto } from './dtos/update-student.dto';
 
 @Injectable()
-export class StudentsService {}
+export class StudentsService {
+  constructor(
+    @Inject(StudentsRepository)
+    private studentsRepository: StudentsRepositoryContract,
+  ) {}
+
+  createStudent(data: CreateStudentDto) {
+    return this.studentsRepository.createStudent(data);
+  }
+
+  listAllStudents() {
+    return this.studentsRepository.getAllStudents();
+  }
+
+  getStudenyByEmail(email: string) {
+    return this.studentsRepository.getStudentByEmail(email);
+  }
+
+  updatePassword(data: UpdateStudentDto, email: string) {
+    return this.studentsRepository.updatePassword(data, email);
+  }
+}
